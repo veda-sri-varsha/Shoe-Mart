40c7bea1122a1de6eab892bdb3bbf14bdae728ce
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-09-04 15:00:54 +0530

    refactor: simplify CustomError class for better readability

--- server/src/services/customError.ts
+++ server/src/services/customError.ts
@@ -1,10 +1,12 @@
 export default class CustomError extends Error {
   statusCode: number;
+  data?: any;
 
-  constructor(statusCode: number, message: string) {
-    super(message);
-    this.statusCode = statusCode;
-    Object.setPrototypeOf(this, new.target.prototype);
-    Error.captureStackTrace(this);
+  constructor(message: string, statusCode: number, data?: any) {
+    super(message);        
+    this.statusCode = statusCode; 
+    this.data = data;     
+
+    Object.setPrototypeOf(this, CustomError.prototype);
   }
 }
