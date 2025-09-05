f9fea91164f9ccece5379806e80e911db9ba924e
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-09-05 16:22:55 +0530

    fix: resolve merge conflict in CustomError

--- server/src/services/customError.ts
+++ server/src/services/customError.ts
@@ -1,24 +1,16 @@
 export default class CustomError extends Error {
   statusCode: number;
-<<<<<<< HEAD
   data?: any;
 
   constructor(message: string, statusCode: number, data?: any) {
-    super(message);        
-    this.statusCode = statusCode; 
-    this.data = data;     
-
-    Object.setPrototypeOf(this, CustomError.prototype);
-=======
-  data: any;
-
-  constructor(message: string, statusCode: number, data: any = null) {
     super(message);
     this.statusCode = statusCode;
     this.data = data;
 
-   Object.setPrototypeOf(this, new.target.prototype);
-    Error.captureStackTrace(this);
->>>>>>> 6e53c6292180d3478127d3733465dc4d2be3c437
+    Object.setPrototypeOf(this, new.target.prototype);
+    
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
