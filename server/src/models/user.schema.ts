6f421243fa5ed16445eb130ed7e97eaa855cdd25
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-09-10 06:44:08 +0530

    feat(user): create user schema with auth methods

--- server/src/models/user.schema.ts
+++ server/src/models/user.schema.ts
@@ -3,7 +3,7 @@ import AuthRoutes from "../constants/authRoles";
 import config from "../config";
 import jwt from "jsonwebtoken";
 import { User } from "../types/index";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import crypto from "crypto";
 
 const userSchema = new mongoose.Schema(
@@ -36,39 +36,37 @@ const userSchema = new mongoose.Schema(
       enum: Object.values(AuthRoutes),
       default: AuthRoutes.USER,
     },
+
     refreshToken: {
       type: String,
-      select: false,
+      select: false, 
     },
+
     isVerified: {
       type: Boolean,
       default: false,
     },
-    verifyOtp: {
-      type: String,
-    },
-    verifyOtpExpireAt: {
-      type: Date,
-    },
-    resetOtp: {
-      type: String,
-    },
-    resetOtpExpireAt: {
-      type: Date,
-    },
+
+    verifyOtp: String,
+    verifyOtpExpireAt: Date,
+    resetOtp: String,
+    resetOtpExpireAt: Date,
+
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
   },
   { timestamps: true }
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-userSchema.methods.comparePassword = async function (enteredPassword: string) {
+userSchema.methods.comparePassword = async function (
+  enteredPassword: string
+) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
@@ -78,16 +76,24 @@ userSchema.methods.generateAccessToken = function () {
   });
 };
 
-userSchema.methods.generateRefreshToken = function (): string {
-  return jwt.sign({ id: this._id }, config.JWT_REFRESH_SECRET, {
+userSchema.methods.generateRefreshToken = async function (): Promise<string> {
+  const token = jwt.sign({ id: this._id }, config.JWT_REFRESH_SECRET, {
     expiresIn: config.JWT_REFRESH_EXPIRATION || "7d",
   });
+
+  this.refreshToken = token; 
+  await this.save({ validateBeforeSave: false });
+
+  return token;
 };
 
-userSchema.methods.generateForgotPasswordToken = function() {
+userSchema.methods.generateForgotPasswordToken = function () {
   const token = crypto.randomBytes(20).toString("hex");
-  this.resetPasswordToken = crypto.createHash("sha256").update(token).digest("hex");
-  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000; // 30 mins
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000; 
   return token;
 };
 
