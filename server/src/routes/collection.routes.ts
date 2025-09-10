31bae410dbbd7ba48700cbf5ed71382e1309d6f3
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-09-10 06:36:33 +0530

    feat(collections): add image upload and update functionality

--- server/src/routes/collection.routes.ts
+++ server/src/routes/collection.routes.ts
@@ -7,12 +7,13 @@ import {
 } from "../controllers/collection.controller";
 import UserAuth, { authorize } from "../middlewares/UserAuth";
 import AuthRoles from "../constants/authRoles";
+import {upload }from "../middlewares/upload.middleware";
 
 const router: Router = Router();
 
 router.get("/", getAllCollections);
-router.post("/", UserAuth, authorize(AuthRoles.ADMIN,AuthRoles.VENDOR),createCollection);
-router.put("/:id", UserAuth, authorize(AuthRoles.ADMIN, AuthRoles.VENDOR),updateCollection);
+router.post("/", UserAuth, authorize(AuthRoles.ADMIN,AuthRoles.VENDOR),upload.single("image"),createCollection);
+router.put("/:id", UserAuth, authorize(AuthRoles.ADMIN, AuthRoles.VENDOR),upload.single("newimage"),updateCollection);
 router.patch("/:id", UserAuth, authorize(AuthRoles.ADMIN, AuthRoles.VENDOR), deleteCollection);
 
 
