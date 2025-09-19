09fadf987603782811a91664b59db1383d2b4209
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-09-19 22:21:40 +0530

    feat(axios): setup axios instance with env baseURL and interceptors

--- client/src/lib/axios-instance.ts
+++ client/src/lib/axios-instance.ts
@@ -1,31 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://some-domain.com/api/',
-  timeout: 1000,
-  headers: {'X-Custom-Header': 'foobar'}
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 5000, 
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Add a request interceptor
-axios.interceptors.request.use(function (config) {
-    // Do something before request is sent
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
-  }, function (error) {
-    // Do something with request error
-    return Promise.reject(error);
   },
-  { synchronous: true, runWhen: () => /* This function returns true */}
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
-// Add a response interceptor
-axios.interceptors.response.use(function onFulfilled(response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  }, function onRejected(error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.error("Unauthorized! Redirecting to login...");
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
     return Promise.reject(error);
-  });
+  }
+);
 
 export default axiosInstance;
